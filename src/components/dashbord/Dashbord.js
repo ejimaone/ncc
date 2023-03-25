527ed7bdcbd8f95dffaa367f259c719c493ff238
ejimaone <abdulwahabonozutu@gmail.com>
2023-03-25 10:13:46 +0100

    align the dashbord

--- src/components/dashbord/Dashbord.js
+++ src/components/dashbord/Dashbord.js
@@ -5,10 +5,10 @@ import SmallCard from "./SmallCard";
 function Dashbord() {
   return (
     <>
-      <h1 className=" lg:w-[71%] mx-auto font-semibold text-2xl text-[#333333] mt-3 pl-7 md:pl-12 md:mt-8 lg:pl-1 ">
+      <h1 className=" lg:w-[71%] mx-auto font-semibold text-2xl text-[#333333] mt-3 pl-7 md:pl-12 md:mt-8 lg:pl-10 ">
         Dashbord
       </h1>
-      <div className="lg:container lg:flex lg:flex-col lg:items-end  -mt-6 lg:-mt-5 ">
+      <div className="lg:container lg:flex lg:flex-col lg:items-end  -mt-6 lg:-mt-5 lg:ml-10">
         <div className="pt-5 lg:flex lg:w-[82%] lg:mx-5  gap-14 lg:gap-6 ">
           <SmallCard />
           <SmallCard />
